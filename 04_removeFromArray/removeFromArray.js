34ab8ceaa72f8245622463ed4fce2dac92d607f9
Raymond <raydhz@pm.me>
2025-02-03 11:12:17 -0800

    Add first function attempt
    first test passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(anArray,...toRemove) {
+    for (let i = toRemove.length; i > 0; i--) {
+        for (let x = anArray.length; x > 0; x--) {
+            if (anArray[x - 1] === toRemove[i - 1]) {
+                anArray.splice(x-1,1);
+            }
+        }
+    }
+    return anArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
