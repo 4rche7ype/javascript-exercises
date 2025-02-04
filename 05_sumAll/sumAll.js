28aa33a71bb128290558a24e0377bb7a22852323
Raymond <raydhz@pm.me>
2025-02-04 12:36:43 -0800

    Add first attempt at solution
    first test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
-};
+const sumAll = function(num1, num2) {
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 > 0 && num2 > 0) {
+            if (num1 < num2) {
+                for (let i = num1 + 1; i <= num2; i++) {
+                    num1 += i;
+                }
+                return num1;
+            } else {
+                for (let i = num2 + 1; i <= num1; i++) {
+                    num2 += i;
+                }
+                return num2;
+            }
+    } else {
+            console.log('ERROR');
+        }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
